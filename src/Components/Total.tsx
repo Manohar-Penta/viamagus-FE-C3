309b80923b62583c77db6bea417a9001c8b8cf29
Manohar <manoharpenta03@gmail.com>
2025-03-05 12:45:10 +0530

    init

---
+++ src/Components/Total.tsx
@@ -0,0 +1,68 @@
+import { Cart } from "../App";
+
+function Total({
+  cart,
+  discount,
+}: {
+  cart: Cart;
+  discount: number | undefined;
+}) {
+  return (
+    <>
+      {Object.keys(cart).length > 0 && (
+        <div
+          className={
+            "border m-2 p-4 lg:p-6 rounded flex flex-col gap-2 max-w-[720px] mx-auto transition-all duration-300 ease-in-out"
+          }
+        >
+          {Object.keys(cart).map((id: string) => {
+            return (
+              <h3 key={id}>
+                <span className="text-xl lg:text-2xl flex justify-between">
+                  <span className="grow">
+                    {cart[Number(id)].name} x {cart[Number(id)].quantity}
+                  </span>{" "}
+                  <span>
+                    ${cart[Number(id)].price * cart[Number(id)].quantity}
+                  </span>
+                </span>
+              </h3>
+            );
+          })}
+          <hr />
+          {discount !== undefined && discount > 0 && (
+            <h3 className="text-xl lg:text-2xl flex justify-between">
+              <span className="grow-[2]">Discount</span>
+              <span>
+                $
+                {Object.keys(cart).reduce(
+                  (acc, id) =>
+                    acc + cart[Number(id)].price * cart[Number(id)].quantity,
+                  0
+                ) *
+                  (discount / 100)}
+              </span>
+            </h3>
+          )}
+          <h1 className="text-xl lg:text-2xl font-semibold flex justify-between">
+            Sub Total
+            <span>
+              $
+              {Object.keys(cart).reduce(
+                (acc, id) =>
+                  acc +
+                  (cart[Number(id)].price *
+                    cart[Number(id)].quantity *
+                    (100 - (discount ?? 0))) /
+                    100,
+                0
+              )}
+            </span>
+          </h1>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Total;
---
+++ src/utils/products.ts
@@ -0,0 +1,27 @@
+type Product = {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+};
+
+export const productList: Product[] = [
+  {
+    id: 1,
+    name: "Product A",
+    price: 50,
+    quantity: 0,
+  },
+  {
+    id: 2,
+    name: "Product B",
+    price: 30,
+    quantity: 0,
+  },
+  {
+    id: 3,
+    name: "Product C",
+    price: 100,
+    quantity: 0,
+  },
+];
